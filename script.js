2dc6e8bd89513fbcb549599712e6ba393469d3b7
san-shor <sanjidaewu89@gmail.com>
2025-09-23 22:58:14 +0600

    Revamp homepage layout

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-import { displayCart } from "./assets/js/cart.js";
+import { displayCart } from './assets/js/cart.js';
 
-const navbarContainer = document.getElementById("navbar-container");
-const cartSlider = document.getElementById("cart-slider");
-const closeIcon = document.getElementById("close-icon");
+const navbarContainer = document.getElementById('navbar-container');
+const cartSlider = document.getElementById('cart-slider');
+const closeIcon = document.getElementById('close-icon');
 
-fetch("header.html")
+fetch('header.html')
   .then((response) => response.text())
   .then((data) => {
     navbarContainer.innerHTML = data;
-    const cartLogo = document.getElementById("cart-logo");
+    const cartLogo = document.getElementById('cart-logo');
 
-    cartLogo.addEventListener("click", function () {
-      cartSlider.classList.toggle("right-slide");
+    cartLogo.addEventListener('click', function () {
+      cartSlider.classList.toggle('right-slide');
     });
 
-    closeIcon.addEventListener("click", function () {
-      cartSlider.classList.remove("right-slide");
+    closeIcon.addEventListener('click', function () {
+      cartSlider.classList.remove('right-slide');
     });
   })
   .catch((error) => {
-    console.error("Error loading the navigation bar:", error);
+    console.error('Error loading the navigation bar:', error);
   });
 
 // Function to display category links dynamically
-const catergoryItems = document.getElementById("category-items");
-let result = "";
+const catergoryItems = document.getElementById('category-items');
+let result = '';
 let existingCategory = [];
 
 async function displayCatergory() {
   try {
-    const response = await fetch("./features/products/category.json");
+    const response = await fetch('./features/products/category.json');
     const catergories = await response.json();
     console.log(catergories);
     catergories.forEach((category) => {
@@ -38,7 +38,7 @@ async function displayCatergory() {
         <div class="category-image">
           <img src="${category.image}" alt="${category.name}">
         </div>
-        ${category.name}
+        <span>${category.name}</span>
       </a>
     </li>
     `;
